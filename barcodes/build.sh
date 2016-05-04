386832594074cb668cb5d3dbd3393f15ad602037
Eric Rasche <rasche.eric@gmail.com>
2016-05-04 15:33:18 +0000

    Fix barcodes

--- barcodes/build.sh
+++ barcodes/build.sh
@@ -3,7 +3,3 @@ go get -v github.com/codegangsta/cli
 go get -v github.com/boombuler/barcode
 go get -v github.com/gorilla/mux
 go get -v github.com/gorilla/handlers
-git clone ${GIT_REPO} /usr/src/myapp
-cd /usr/src/myapp && git checkout ${GIT_VERSION}
-go build -v  -ldflags "-s" -a -installsuffix cgo -o /out/main
-chown 1000:1000 /out/main
