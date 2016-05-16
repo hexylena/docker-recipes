056f0d5605516d5f4569923a305053b92b797c19
Eric Rasche <rasche.eric@gmail.com>
2016-05-16 23:34:12 +0000

    Switch around import order

--- galaxy-apollo/galaxy/postinst.sh
+++ galaxy-apollo/galaxy/postinst.sh
@@ -2,3 +2,5 @@
 
 . $GALAXY_VIRTUALENV/bin/activate
 pip install bcbio-gff biopython
+sed -i 's|/opt/apollo/data/galaxy/|/apollo-data/|g' tools/apollo/*.xml
+mkdir /apollo-data/ && chmod 777 /apollo-data/
