87535f7ccfc5d3866b86239bf4bfb44e0bb1564b
Eric Rasche <rasche.eric@gmail.com>
2016-05-23 21:16:40 +0000

    Wipe out old image

--- galaxy-apollo/galaxy/postinst.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-. $GALAXY_VIRTUALENV/bin/activate
-pip install bcbio-gff biopython
-sed -i 's|/opt/apollo/data/galaxy/|/apollo-data/|g' tools/apollo/*.xml
-mkdir /apollo-data/ && chmod 777 /apollo-data/
